42c0d12a6fa2d97ddfd6aaf8e1bdb6f88eb58b7e
NChaouch <nourchaouch44@gmail.com>
2024-11-18 12:22:13 +0100

    change task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,16 +1,7 @@
 function getListStudentIds(array) {
-  if (array == null) {
+  if (!Array.isArray(array)) {
     return [];
   }
   return array.map((student) => student.id);
 }
-const students = [
-  { id: 1, name: 'John' },
-  { id: 2, name: 'Jane' },
-  { id: 5, name: 'Joe' },
-];
-
-console.log(getListStudentIds(null));
-console.log(getListStudentIds(students));
-
 module.exports = getListStudentIds;
