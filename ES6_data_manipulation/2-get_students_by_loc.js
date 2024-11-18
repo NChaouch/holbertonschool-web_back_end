34416fd18f28d8a0ac8d2a4487fa8b2b77db9a33
NChaouch <nourchaouch44@gmail.com>
2024-11-18 12:35:37 +0100

    2.filter

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+function getStudentsByLocation() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
+
+module.exports = getStudentsByLocation;
