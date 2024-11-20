8339ad7969b8fa0f64d2dec09d401e84c1c253c6
NChaouch <nourchaouch44@gmail.com>
2024-11-20 11:04:54 +0100

    8. Throw an error

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+function divideFunction(numerator, deominator) {
+  if (deominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / deominator;
+}
+
+module.exports = divideFunction;
