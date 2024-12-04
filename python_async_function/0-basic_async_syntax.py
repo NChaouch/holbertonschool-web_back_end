22064db110b4c76d214c32d69a9ab245afc37113
NChaouch <nourchaouch44@gmail.com>
2024-12-04 12:20:04 +0100

    0. The basics of async pycodestyle

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -11,6 +11,7 @@ asyncio for managing asynchronism and breaks
 non-blocking.
 """
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     generates a random num between 0 and max_delay.
