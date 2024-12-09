33533291c62c83c83828928c5c407a5da30dae95
NChaouch <nourchaouch44@gmail.com>
2024-12-09 19:56:23 +0100

    1. Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-
 """
-module.
+Module.
 """
-
 
 import csv
+import math
 from typing import List
 index_range = __import__('0-simple_helper_function').index_range
 
@@ -16,32 +15,33 @@ class Server:
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
-
     def __init__(self):
         self.__dataset = None
-
 
     def dataset(self) -> List[List]:
         """
         Cached dataset
         """
         if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+                with open(self.DATA_FILE) as f:
+                    reader = csv.reader(f)
+                    dataset = [row for row in reader]
+                self.__dataset = dataset[1:]
 
         return self.__dataset
 
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0  # if page is (int)
-        # if page_size is (int)
+        """
+        Returns page (dataset).
+        """
+        assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-        # calculate start and end indices
+        # Calculate start_index and end_index
         start_index, end_index = index_range(page, page_size)
-        # retrieves complete dataset
+        # Retrieve the dataset
         dataset = self.dataset()
+        # Return the page dataset or empty list
         if start_index >= len(dataset):
             return []
-        return dataset[start_index: end_index]
+        return dataset[start_index:end_index]
+
