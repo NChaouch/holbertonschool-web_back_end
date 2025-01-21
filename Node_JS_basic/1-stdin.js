c1ace13584f593eb1eb9716c0395ceb571f45a0b
NChaouch <nourchaouch44@gmail.com>
2025-01-21 14:15:04 +0100

    task 1 correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,9 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // datas by user
 process.stdin.on('data', (data) => {
   const name = data.toString().trim(); // delete spaces around name
-  console.log(`Your name is: ${name}`); // display name of user
+  process.stdout.write(`Your name is: ${name}`); // display name of user
 
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing');
 
   // end of the processus
   process.stdin.end();
