87a94d9a1ff9106716ea054af1582e71920f3bdc
NChaouch <nourchaouch44@gmail.com>
2025-01-21 19:21:17 +0100

    task 1 correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,12 +5,12 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 rl.on('line', (name) => {
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
 
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 
   rl.close();
 });
