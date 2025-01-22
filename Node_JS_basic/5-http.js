d15c6a3a868a525c9a2d86ce48ef66323c880559
NChaouch <nourchaouch44@gmail.com>
2025-01-22 15:55:42 +0100

    correction

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,8 @@
-const http = require('express');
+const express = require('express');
+const app = express();
 // creation of http
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 // server in 1245
 app.listen(1245);
