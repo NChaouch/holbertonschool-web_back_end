1ad52ebf891fe2f85c0d0f511af1af658a00b46c
NChaouch <nourchaouch44@gmail.com>
2025-01-22 15:46:48 +0100

    5. Create a more complex HTTP server using Node's HTTP module

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,10 @@
+const http = require('express');
+// creation of http
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+// server in 1245
+app.listen(1245);
+// export the server
+module.exports = app;
