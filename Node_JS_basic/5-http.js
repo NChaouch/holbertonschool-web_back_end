a8a9e14a9bb8bbae66a104395c4b9ea2f3c7b2b7
NChaouch <nourchaouch44@gmail.com>
2025-01-24 10:24:22 +0100

    correction

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app = express();
-// creation of http
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } 
+  
+  if (req.url === '/students') {
+    const dbPath = process.argv[2];
+    
+    res.write('This is the list of our students\n');
+    
+    countStudents(dbPath)
+      .then(() => res.end())
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end(error.message);
+      });
+  }
 });
-// server in 1245
+
 app.listen(1245);
-// export the server
+
 module.exports = app;
